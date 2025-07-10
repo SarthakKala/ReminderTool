88d6028a2d1a05ea59d5ed950e47593a4c7ceda8
SarthakKala <35kalasarthak@gmail.com>
2025-07-10 14:24:55 +0530

    Created the basic reminder tool functionality.

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/gen2brain/beeep"
+	"github.com/olebedev/when"
+	"github.com/olebedev/when/rules/common"
+	"github.com/olebedev/when/rules/en"
+)
+
+const (
+	markName  = "GOLANG_CLI_REMINDER"
+	markValue = "1"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %s <hh:mm> <Text Message>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	now := time.Now()
+
+	w := when.New(nil)
+	w.Add(en.All...)
+	w.Add(common.All...)
+
+	t, err := w.Parse(os.Args[1], now)
+	if err != nil {
+		fmt.Printf("Error parsing time: %v\n", err)
+		os.Exit(2)
+	}
+	if t == nil {
+		fmt.Println("Could not parse time, please use the format hh:mm")
+		os.Exit(2)
+	}
+	if now.After(t.Time) {
+		fmt.Println("The time you entered is in the past, please enter a future time.")
+		os.Exit(3)
+	}
+
+	diff := t.Time.Sub(now)
+	if os.Getenv(markName) == markValue {
+		time.Sleep(diff)
+		err := beeep.Alert("Reminder-by CLI", strings.Join(os.Args[2:], " "), "/testdata/info.png")
+		if err != nil {
+			fmt.Printf("Error sending alert: %v\n", err)
+			os.Exit(4)
+		}
+	} else {
+		cmd := exec.Command(os.Args[0], os.Args[1:]...)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", markName, markValue))
+		if err := cmd.Start(); err != nil {
+			fmt.Printf("Error starting reminder: %v\n", err)
+			os.Exit(5)
+		}
+		fmt.Println("Reminder will be displayed after" , diff.Round(time.Second))
+		os.Exit(0)
+	}
+
+}
